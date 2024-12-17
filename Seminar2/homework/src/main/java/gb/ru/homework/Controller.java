d259621c0730d6353f01bb5178f918db53d0f0a7
Valentin developer <buziza@yandex.ru>
2024-12-17 13:11:48 +0300

    home work seminar 2 step 2

--- Seminar2/homework/src/main/java/gb/ru/homework/Controller.java
+++ Seminar2/homework/src/main/java/gb/ru/homework/Controller.java
@@ -1,12 +1,19 @@
 package gb.ru.homework;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
+    private final GenerateStringService generateStringService;
     @GetMapping("/homework")
-    public String hello(){
-        return "Привет! Это домашнее задание к семинару 2!";
+    public String saySomething(){
+        return generateStringService.getSomeString();
+    }
+    @Autowired
+    public Controller(GenerateStringService generateStringService) {
+        this.generateStringService = generateStringService;
     }
 }
---
+++ Seminar2/homework/src/main/java/gb/ru/homework/GenerateStringService.java
@@ -0,0 +1,9 @@
+package gb.ru.homework;
+
+import org.springframework.stereotype.Service;
+@Service
+public class GenerateStringService {
+    public String getSomeString(){
+        return "Привет! Это домашнее задание к семинару 2!";
+    }
+}
